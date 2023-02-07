5901eb8addffd8b0d3c9154c0149a035d5612a3f
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-02-07 22:08:26 +0530

    trading functions added

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.17;
 
 import "./FunToken.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Marketplace is Ownable {
@@ -20,12 +19,12 @@ contract Marketplace is Ownable {
     uint256 public weiRaised;
 
     /**
-    * Event for token purchase logging.
-    * @param buyer who paid for the tokens
-    * @param beneficiary who got the tokens
-    * @param value weis paid for purchase
-    * @param tokenAmount amount of tokens purchased
-    */
+     * Event for token purchase logging.
+     * @param buyer who paid for the tokens
+     * @param beneficiary who got the tokens
+     * @param value weis paid for purchase
+     * @param tokenAmount amount of tokens purchased
+     */
     event TokenPurchase(
         address indexed buyer,
         address indexed beneficiary,
@@ -33,40 +32,48 @@ contract Marketplace is Ownable {
         uint256 tokenAmount
     );
 
+    event TokenSell(
+        address indexed seller,
+        uint256 amountOfMATICToTransfer,
+        uint256 tokenAmountToSell
+    );
+
     /**
-    * @param _rate number of tokens units a buyer gets per wei
-    * @param _wallet address where collected funds will be forwarded to
-    * @param _token address of token being sold
-    */
+     * @param _rate number of tokens units a buyer gets per wei
+     * @param _wallet address where collected funds will be forwarded to
+     * @param _token address of token being sold
+     */
     constructor(uint256 _rate, address payable _wallet, FunToken _token) {
         require(_rate > 0);
         require(_wallet != address(0));
 
         rate = _rate;
-        wallet = _wallet;   
+        wallet = _wallet;
         token = _token;
     }
 
     /**
-    * @dev fallback function
-    */
+     * @dev fallback function
+     */
     fallback() external payable {
         buyTokens(msg.sender);
     }
 
     /**
-    * @dev receive ether function
-    */
+     * @dev receive ether function
+     */
     receive() external payable {
         buyTokens(msg.sender);
     }
 
     /**
-    * @dev Low level token purchase.
-    * @param _buyer address performing the token purchase
-    * @return _tokenAmount number of tokens the user purchased
-    */
-    function buyTokens(address _buyer) public payable returns (uint256 _tokenAmount) {
+     * @dev Low level token purchase.
+     * @param _buyer address performing the token purchase
+     * @return _tokenAmount number of tokens the user purchased
+     */
+    function buyTokens(
+        address _buyer
+    ) public payable returns (uint256 _tokenAmount) {
         uint256 weiAmount = msg.value;
 
         // Calculate token amount to be created
@@ -79,12 +86,7 @@ contract Marketplace is Ownable {
 
         _processPurchase(_buyer, tokenAmount);
 
-        emit TokenPurchase(
-            msg.sender,
-            _buyer,
-            weiAmount,
-            tokenAmount
-        );
+        emit TokenPurchase(msg.sender, _buyer, weiAmount, tokenAmount);
 
         _updatePurchasingState(_buyer, weiAmount);
 
@@ -95,97 +97,128 @@ contract Marketplace is Ownable {
     }
 
     /**
-    * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are met.
-    * @param _buyer address performing the token purchase
-    * @param _weiAmount value in wei involved in the purchase
-    */
-    function _preValidatePurchase(address _buyer, uint256 _weiAmount, uint256 tokenAmount) internal view {
+     * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are met.
+     * @param _buyer address performing the token purchase
+     * @param _weiAmount value in wei involved in the purchase
+     */
+    function _preValidatePurchase(
+        address _buyer,
+        uint256 _weiAmount,
+        uint256 tokenAmount
+    ) internal view {
+        // Also add logic to confirm that user sends MATIC
         require(_buyer != address(0));
         require(_weiAmount > 0, "You need to send some MATIC to proceed");
 
         uint256 marketplaceBalance = token.balanceOf(address(this));
-        require(marketplaceBalance >= tokenAmount, "Marketplace has insufficient tokens");
+        require(
+            marketplaceBalance >= tokenAmount,
+            "Marketplace has insufficient tokens"
+        );
     }
 
     /**
-    * @dev Validation of an executed purchase. Observe state and use revert statements to undo rollback when valid conditions are not met.
-    * @param _buyer address performing the token purchase
-    * @param _weiAmount value in wei involved in the purchase
-    */
-    function _postValidatePurchase(address _buyer, uint256 _weiAmount) internal {}
+     * @dev Validation of an executed purchase. Observe state and use revert statements to undo rollback when valid conditions are not met.
+     * @param _buyer address performing the token purchase
+     * @param _weiAmount value in wei involved in the purchase
+     */
+    function _postValidatePurchase(
+        address _buyer,
+        uint256 _weiAmount
+    ) internal {}
 
     /**
-    * @dev Source of tokens. Override this method to modify the way in which the Marketplace ultimately gets and sends its tokens.
-    * @param _buyer address performing the token purchase
-    * @param _tokenAmount number of tokens to be emitted
-    */
+     * @dev Source of tokens. Override this method to modify the way in which the Marketplace ultimately gets and sends its tokens.
+     * @param _buyer address performing the token purchase
+     * @param _tokenAmount number of tokens to be emitted
+     */
     function _deliverTokens(address _buyer, uint256 _tokenAmount) internal {
-        (bool sent) = token.transfer(_buyer, _tokenAmount);
+        bool sent = token.transfer(_buyer, _tokenAmount);
         require(sent, "Failed to transfer tokens to user");
     }
 
     /**
-    * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
-    * @param _buyer address receiving the tokens
-    * @param _tokenAmount number of tokens to be purchased
-    */
+     * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+     * @param _buyer address receiving the tokens
+     * @param _tokenAmount number of tokens to be purchased
+     */
     function _processPurchase(address _buyer, uint256 _tokenAmount) internal {
         _deliverTokens(_buyer, _tokenAmount);
     }
 
     /**
-    * @dev Override for extensions that require an internal state to check for validity. (current user contributions, etc.)
-    * @param _buyer address receiving the tokens
-    * @param _weiAmount value in wei involved in the purchase
-    */
-    function _updatePurchasingState(address _buyer, uint256 _weiAmount) internal {}
+     * @dev Override for extensions that require an internal state to check for validity. (current user contributions, etc.)
+     * @param _buyer address receiving the tokens
+     * @param _weiAmount value in wei involved in the purchase
+     */
+    function _updatePurchasingState(
+        address _buyer,
+        uint256 _weiAmount
+    ) internal {}
 
     /**
-    * @dev Override to extend the way in which ether is converted into tokens.
-    * @param _weiAmount value in wei to be converted into tokens
-    * @return number of tokens that can be purchased with the specified _weiAmount
-    */
-    function _getTokenAmount(uint256 _weiAmount) internal view returns(uint256) {
-        return _weiAmount*rate;
+     * @dev Override to extend the way in which MATIC is converted into tokens.
+     * @param _weiAmount value in wei to be converted into tokens
+     * @return number of tokens that can be purchased with the specified _weiAmount
+     */
+    function _getTokenAmount(
+        uint256 _weiAmount
+    ) internal view returns (uint256) {
+        return _weiAmount * rate;
     }
 
     /**
-    * @dev Determines how ETH is stored/forwarded on purchases.
-    */
+     * @dev Determines how MATIC is stored/forwarded on purchases.
+     */
     function _forwardFunds() internal {
-        wallet.transfer(msg.value);
+        // wallet.transfer(msg.value);
     }
 
     /**
-    * @dev Low level token selling.
-    * @param tokenAmountToSell amount of tokens the user wants to sell
-    */
+     * @dev Low level token selling. To sell tokens to Marketplace the user must allow Marketplace to transfer the amount of tokens the user wants to sell
+     * @param tokenAmountToSell amount of tokens the user wants to sell
+     */
     function sellTokens(uint256 tokenAmountToSell) public {
+        require(
+            tokenAmountToSell > 0,
+            "Specify an amount of token greater than zero"
+        );
 
-    require(tokenAmountToSell > 0, "Specify an amount of token greater than zero");
+        uint256 userBalance = token.balanceOf(msg.sender);
+        require(
+            userBalance >= tokenAmountToSell,
+            "You have insufficient tokens"
+        );
 
-    uint256 userBalance = token.balanceOf(msg.sender);
-    require(userBalance >= tokenAmountToSell, "You have insufficient tokens");
+        uint256 amountOfMATICToTransfer = tokenAmountToSell / rate;
+        uint256 marketplaceMATICBalance = address(this).balance;
+        require(
+            marketplaceMATICBalance >= amountOfMATICToTransfer,
+            "Marketplace has insufficient funds"
+        );
 
-    uint256 amountOfMATICToTransfer = tokenAmountToSell / rate;
-    uint256 marketplaceMATICBalance = address(this).balance;
-    require(marketplaceMATICBalance >= amountOfMATICToTransfer, "Marketplace has insufficient funds");
+        bool sent = token.transferFrom(
+            msg.sender,
+            address(this),
+            tokenAmountToSell
+        );
+        require(sent, "Failed to transfer tokens from user to Marketplace");
 
-    (bool sent) = token.transferFrom(msg.sender, address(this), tokenAmountToSell);
-    require(sent, "Failed to transfer tokens from user to Marketplace");
+        (sent, ) = msg.sender.call{value: amountOfMATICToTransfer}("");
+        require(sent, "Failed to send MATIC to the user");
 
-    (sent,) = msg.sender.call{value: amountOfMATICToTransfer}("");
-    require(sent, "Failed to send MATIC to the user");
-  }
+        emit TokenSell(msg.sender, amountOfMATICToTransfer, tokenAmountToSell);
+    }
 
-  /**
-   * @dev This function allows the owner to send all the MATIC stored in the Marketplace smart contract into the owner’s wallet.
-   */
-  function withdraw() public onlyOwner {
-    uint256 marketplaceBalance = address(this).balance;
-    require(marketplaceBalance > 0, "No MATIC present in Marketplace");
+    /**
+     * @dev This function can only be run by the owner of the contract.
+     * This function allows the owner to send all the MATIC stored in the Marketplace smart contract into the owner’s wallet.
+     */
+    function withdraw() public onlyOwner {
+        uint256 marketplaceBalance = address(this).balance;
+        require(marketplaceBalance > 0, "No MATIC present in Marketplace");
 
-    (bool sent,) = msg.sender.call{value: address(this).balance}("");
-    require(sent, "Failed to withdraw");
-  }
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to withdraw");
+    }
 }
--- test/Marketplace.js
+++ test/Marketplace.js
@@ -1,8 +1,10 @@
 const { expect } = require("chai");
 
 describe('Staking', () => {
-  beforeEach(async () => {
+  before(async () => {
     [owner, address1, address2] = await ethers.getSigners();
+
+    console.log("Owner MATIC Balance Before Deploying Contracts: ", ethers.utils.formatEther(await owner.getBalance()));
 
     FunToken = await ethers.getContractFactory('FunToken', owner);
     funToken = await FunToken.deploy();
@@ -13,37 +15,216 @@ describe('Staking', () => {
 
 
   describe('buyTokens', () => {
-    it('adds a token symbol', async () => {
+    it('Should let us buy tokens and our balance should go up.', async () => {
       let totalSupply;
-      let address1Balance;
-      let address2Balance;
+      let address1TokenBalance;
+      let address2TokenBalance;
+
+      console.log("Owner Address: ", owner.address);
+      console.log('Marketplace address: ', marketplace.address);
+      console.log('FunToken address: ', funToken.address);
+
+      console.log("Owner MATIC Balance After Deploying Contracts: ", ethers.utils.formatEther(await owner.getBalance()));
 
       totalSupply = await funToken.totalSupply();
-      address1Balance = await funToken.balanceOf(address1.address);
-      address2Balance = await funToken.balanceOf(address2.address);
+      address1TokenBalance = await funToken.balanceOf(address1.address);
+      address2TokenBalance = await funToken.balanceOf(address2.address);
       expect(totalSupply).to.be.equal(ethers.utils.parseEther('10000000'));
-      expect(address1Balance).to.be.equal(0);
-      expect(address2Balance).to.be.equal(0);
+      expect(address1TokenBalance).to.be.equal(0);
+      expect(address2TokenBalance).to.be.equal(0);
+
+      console.log("Owner MATIC Balance Before Minting Tokens to Marketplace Contract: ", ethers.utils.formatEther(await owner.getBalance()));
 
       await funToken.connect(owner).mint(
         marketplace.address,
         ethers.utils.parseEther('10000')
       );
 
-      const ownerEtherBalanceOld = await owner.getBalance();
+      console.log("Owner MATIC Balance After Minting Tokens to Marketplace Contract: ", ethers.utils.formatEther(await owner.getBalance()));
 
+      const startingOwnerTokenBalance = await funToken.balanceOf(owner.address);
+      console.log('\t', "Starting Owner Token balance: ", ethers.utils.formatEther(startingOwnerTokenBalance));
+
+      const startingOwnerMATICBalance = await owner.getBalance();
+      console.log('\t', "Starting Owner MATIC balance: ", ethers.utils.formatEther(startingOwnerMATICBalance));
+
+      const startingMarketplaceTokenBalance = await funToken.balanceOf(marketplace.address);
+      console.log('\t', "Starting Marketplace Token balance: ", ethers.utils.formatEther(startingMarketplaceTokenBalance));
+
+      const startingMarketplaceMATICBalance = await ethers.provider.getBalance(marketplace.address);
+      console.log('\t', "Starting Marketplace MATIC balance: ", ethers.utils.formatEther(startingMarketplaceMATICBalance));
+
+      console.log('\t', "Buying...");
       await marketplace.connect(address1).buyTokens(address1.address, {value: ethers.utils.parseEther('10')});
       await marketplace.connect(address2).buyTokens(address2.address, {value: ethers.utils.parseEther('20')});
 
-      totalSupply = await mpToken.totalSupply();
-      address1Balance = await funToken.connect(owner).balanceOf(address1.address);
-      address2Balance = await funToken.connect(owner).balanceOf(address2.address);
-      const ownerEtherBalanceNew = await owner.getBalance();
+      console.log('\t', "Waiting for confirmation...");
+
+      totalSupply = await funToken.totalSupply();
+      address1TokenBalance = await funToken.connect(owner).balanceOf(address1.address);
+      address2TokenBalance = await funToken.connect(owner).balanceOf(address2.address);
+
+      const newOwnerTokenBalance = await funToken.balanceOf(owner.address);
+      console.log('\t', "New Owner Token balance: ", ethers.utils.formatEther(newOwnerTokenBalance));
+
+      const newOwnerMATICBalance = await owner.getBalance();
+      console.log('\t', "New Owner MATIC balance: ", ethers.utils.formatEther(newOwnerMATICBalance));
+
+      const newMarketplaceTokenBalance = await funToken.balanceOf(marketplace.address);
+      console.log('\t', "New Marketplace Token balance: ", ethers.utils.formatEther(newMarketplaceTokenBalance));
+
+      const newMarketplaceMATICBalance = await ethers.provider.getBalance(marketplace.address);
+      console.log('\t', "New Marketplace MATIC balance: ", ethers.utils.formatEther(newMarketplaceMATICBalance));
+
+      console.log('\t', "Address1 Token balance: ", ethers.utils.formatEther(address1TokenBalance));
+      console.log('\t', "Address2 Token balance: ", ethers.utils.formatEther(address2TokenBalance));
 
       expect(totalSupply).to.be.equal(ethers.utils.parseEther('10010000'));
-      expect(address1Balance).to.be.equal(ethers.utils.parseEther('20'));
-      expect(address2Balance).to.be.equal(ethers.utils.parseEther('40'));
-      expect(ownerEtherBalanceNew).to.be.above(ownerEtherBalanceOld);
+      expect(address1TokenBalance).to.be.equal(ethers.utils.parseEther('20'));
+      expect(address2TokenBalance).to.be.equal(ethers.utils.parseEther('40'));
+      // expect(newOwnerMATICBalance).to.be.above(startingOwnerMATICBalance);
+      expect(newMarketplaceTokenBalance).to.be.below(startingMarketplaceTokenBalance);
+    });
+  });
+
+  describe("sellTokens()", () => {
+    it("Should let us sell tokens and we should get the appropriate amount of MATIC back.", async function() {
+
+      console.log('Owner address: ', owner.address);
+      console.log('Marketplace address: ', marketplace.address);
+      console.log('FunToken address: ', funToken.address);
+
+      const startingOwnerTokenBalance = await funToken.balanceOf(owner.address);
+      console.log('\t', "Starting Owner Token balance: ", ethers.utils.formatEther(startingOwnerTokenBalance));
+
+      const startingOwnerMATICBalance = await ethers.provider.getBalance(owner.address);
+      console.log('\t', "Starting Owner MATIC balance: ", ethers.utils.formatEther(startingOwnerMATICBalance));
+
+      const startingMarketplaceTokenBalance = await funToken.balanceOf(marketplace.address);
+      console.log('\t', "Starting Marketplace Token balance: ", ethers.utils.formatEther(startingMarketplaceTokenBalance));
+
+      const startingMarketplaceMATICBalance = await ethers.provider.getBalance(marketplace.address);
+      console.log('\t', "Starting Marketplace MATIC balance: ", ethers.utils.formatEther(startingMarketplaceMATICBalance));
+
+      const startingAddress1TokenBalance = await funToken.balanceOf(address1.address);
+      console.log('\t', "Address1 Token balance: ", ethers.utils.formatEther(startingAddress1TokenBalance));
+
+      console.log('\t', "Approving...")
+      const approveTokensResult = await funToken.connect(address1).approve(marketplace.address, ethers.utils.parseEther("10"));
+      console.log('\t', "approveTokens Result: ", approveTokensResult.hash);
+
+      console.log('\t', "Waiting for confirmation...");
+      const atxResult =  await approveTokensResult.wait();
+      expect(atxResult.status).to.equal(1, "Error when expecting the transaction result to equal 1");
+
+      console.log('\t', "Selling...");
+      
+      const sellTokensResult = await marketplace.connect(address1).sellTokens(ethers.utils.parseEther("10"));
+      console.log('\t', "sellTokens Result: ", sellTokensResult.hash);
+
+      console.log('\t', "Waiting for confirmation...");
+      const txResult =  await sellTokensResult.wait();
+      expect(txResult.status).to.equal(1, "Error when expecting the transaction status to equal 1");
+
+      const newOwnerTokenBalance = await funToken.balanceOf(owner.address);
+      console.log('\t', "New Owner Token balance: ", ethers.utils.formatEther(newOwnerTokenBalance));
+
+      const newOwnerMATICBalance = await ethers.provider.getBalance(owner.address);
+      console.log('\t', "New Owner MATIC balance: ", ethers.utils.formatEther(newOwnerMATICBalance));
+
+      const newMarketplaceTokenBalance = await funToken.balanceOf(marketplace.address);
+      console.log('\t', "New Marketplace Token balance: ", ethers.utils.formatEther(newMarketplaceTokenBalance));
+
+      const newMarketplaceMATICBalance = await ethers.provider.getBalance(marketplace.address);
+      console.log('\t', "New Marketplace MATIC balance: ", ethers.utils.formatEther(newMarketplaceMATICBalance));
+
+      const newAddress1TokenBalance = await funToken.balanceOf(address1.address);
+      console.log('\t', "Address1 Token balance: ", ethers.utils.formatEther(newAddress1TokenBalance));
+
+      expect(newAddress1TokenBalance).to.equal(startingAddress1TokenBalance.sub(ethers.utils.parseEther("10")), "Error when expecting the token balance to have increased by 10");
+
+      const maticChange = startingMarketplaceMATICBalance.sub(newMarketplaceMATICBalance);
+      expect(maticChange).to.equal(ethers.utils.parseEther('5'), "Error when expecting the MATIC returned to the user by the sellTokens function to be correct");
+    });
+  });
+
+  describe("withdraw()", () => {
+    it("Should let the owner (and nobody else) withdraw the MATIC from the contract.", async function() {
+      const [ owner, nonOwner ] = await ethers.getSigners();
+
+      console.log('Owner address: ', owner.address);
+      console.log('Non-owner address: ', nonOwner.address);
+      console.log('Marketplace address: ', marketplace.address);
+      console.log('FunToken address: ', funToken.address);
+
+      const beforeBuyingMarketplaceTokenBalance = await funToken.balanceOf(marketplace.address);
+      console.log('\t', "Before Buying Marketplace Token balance: ", ethers.utils.formatEther(beforeBuyingMarketplaceTokenBalance));
+
+      const beforeBuyingMarketplaceMATICBalance = await ethers.provider.getBalance(marketplace.address);
+      console.log('\t', "Before Buying Marketplace MATIC balance: ", ethers.utils.formatEther(beforeBuyingMarketplaceMATICBalance));
+
+      const beforeBuyingNonOwnerTokenBalance = await funToken.balanceOf(nonOwner.address);
+      console.log('\t', "Before Buying Non-owner Token balance: ", ethers.utils.formatEther(beforeBuyingNonOwnerTokenBalance));
+
+      const beforeBuyingNonOwneMATICBalance = await ethers.provider.getBalance(nonOwner.address);
+      console.log('\t', "Before Buying Non-owner MATIC balance: ", ethers.utils.formatEther(beforeBuyingNonOwneMATICBalance));
+
+      console.log('\t', "Buying some tokens...");
+      const buyTokensResult = await marketplace.connect(nonOwner).buyTokens(nonOwner.address, {value: ethers.utils.parseEther("5")});
+      console.log('\t', "buyTokens Result: ", buyTokensResult.hash);
+
+      console.log('\t', "Waiting for confirmation...");
+      const buyTxResult =  await buyTokensResult.wait();
+      expect(buyTxResult.status).to.equal(1, "Error when expecting the transaction result to be 1");
+
+      const afterBuyingMarketplaceTokenBalance = await funToken.balanceOf(marketplace.address);
+      console.log('\t', "After Buying Marketplace Token balance: ", ethers.utils.formatEther(afterBuyingMarketplaceTokenBalance));
+
+      const afterBuyingMarketplaceMATICBalance = await ethers.provider.getBalance(marketplace.address);
+      console.log('\t', "After Buying Marketplace MATIC balance: ", ethers.utils.formatEther(afterBuyingMarketplaceMATICBalance));
+
+      const afterBuyingNonOwnerTokenBalance = await funToken.balanceOf(nonOwner.address);
+      console.log('\t', "After Buying Non-owner Token balance: ", ethers.utils.formatEther(afterBuyingNonOwnerTokenBalance));
+
+      const afterBuyingNonOwneMATICBalance = await ethers.provider.getBalance(nonOwner.address);
+      console.log('\t', "After Buying Non-owner MATIC balance: ", ethers.utils.formatEther(afterBuyingNonOwneMATICBalance));
+
+      const startingMarketplaceMATICBalance = await ethers.provider.getBalance(marketplace.address);
+      console.log('\t', "Starting Marketplace MATIC balance: ", ethers.utils.formatEther(startingMarketplaceMATICBalance));
+
+      console.log('\t', "Withdrawing as non-owner (should fail)...");
+
+      const startingNonOwnerMATICBalance = await ethers.provider.getBalance(nonOwner.address);
+      console.log('\t', "Starting Non-owner MATIC balance: ", ethers.utils.formatEther(startingNonOwnerMATICBalance));
+
+      await expect(marketplace.connect(nonOwner).withdraw()).to.be.reverted;
+      console.log('\t', "Withdraw reverted as non-owner");
+
+      const newNonOwnerMATICBalance = await ethers.provider.getBalance(nonOwner.address);
+      console.log('\t', "New Non-owner MATIC balance: ", ethers.utils.formatEther(newNonOwnerMATICBalance));
+      expect(newNonOwnerMATICBalance).to.be.lte(startingNonOwnerMATICBalance, "Error when expecting the new MATIC balance to be <= to the previous balance after calling withdraw by a non owner");
+
+      console.log('\t', "Withdrawing as owner...");
+      const startingOwnerMATICBalance = await ethers.provider.getBalance(owner.address);
+      console.log('\t', "Starting Owner MATIC balance: ", ethers.utils.formatEther(startingOwnerMATICBalance));
+      const withdrawResult = await marketplace.withdraw();
+      console.log('\t', "Withdraw Result: ", withdrawResult.hash);
+
+      console.log('\t',"Waiting for confirmation...");
+      const withdrawTxResult =  await withdrawResult.wait();
+      expect(withdrawTxResult.status).to.equal(1, "Error when expecting the withdraw transaction to equal 1");
+
+      const newOwnerMATICBalance = await ethers.provider.getBalance(owner.address);
+      console.log('\t', "New Owner MATIC balance: ", ethers.utils.formatEther(newOwnerMATICBalance));
+
+      const newMarketplaceMATICBalance = await ethers.provider.getBalance(marketplace.address);
+      console.log('\t', "New Marketplace MATIC balance: ", ethers.utils.formatEther(newMarketplaceMATICBalance));
+
+      const tx = await ethers.provider.getTransaction(withdrawResult.hash);
+      const receipt = await ethers.provider.getTransactionReceipt(withdrawResult.hash);
+      const gasCost = tx.gasPrice?.mul(receipt.gasUsed);
+
+      expect(newOwnerMATICBalance).to.equal(startingOwnerMATICBalance.add(startingMarketplaceMATICBalance).sub(ethers.BigNumber.from(gasCost)), "Error when expecting the owner's MATIC returned by withdraw to be sufficient");
     });
   });
 });
